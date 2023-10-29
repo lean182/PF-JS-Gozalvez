87a66e634d30aa09e891180463b3e263a5085976
Leandro Gozalvez <goz_leandro@hotmail.com>
2023-10-29 15:54:49 -0300

    cuarto comentario, se agregan comentarios al .html, .css y .js

--- script.js
+++ script.js
@@ -1,31 +1,40 @@
 class Calculadora {
   constructor() {
+    // Propiedades iniciales
     this.entradaActual = "";
     this.resultado = 0;
     this.estaParentesisAbierto = true;
+
+    // Elementos del DOM
     this.pantalla = document.getElementById("pantalla");
     this.historialEcuaciones =
       JSON.parse(localStorage.getItem("historial")) || [];
+
+    // Inicializar eventos
     this.inicializarEventos();
     this.actualizarHistorial();
   }
 
+  // Agregar un valor a la pantalla
   agregarAPantalla(valor) {
     this.entradaActual += valor;
     this.pantalla.value = this.entradaActual;
   }
 
+  // Limpiar la pantalla
   limpiarPantalla() {
     this.entradaActual = "";
     this.resultado = 0;
     this.pantalla.value = "";
   }
 
+  // Realizar cálculos
   calcular(expr) {
     expr = expr.replace(/x/g, "*").replace(/÷/g, "/");
     let operandos = expr.split(/[\+\-\*\/]/).map((o) => parseFloat(o));
     let operaciones = expr.match(/[\+\-\*\/]/g) || [];
 
+    // Realizar multiplicación y división
     while (operaciones.includes("*") || operaciones.includes("/")) {
       let i = operaciones.findIndex((o) => o === "*" || o === "/");
       operandos[i] =
@@ -36,6 +45,7 @@ class Calculadora {
       operaciones.splice(i, 1);
     }
 
+    // Realizar suma y resta
     while (operaciones.includes("+") || operaciones.includes("-")) {
       let i = operaciones.findIndex((o) => o === "+" || o === "-");
       operandos[i] =
@@ -49,6 +59,7 @@ class Calculadora {
     return operandos[0];
   }
 
+  // Calcular y mostrar el resultado
   calcularResultado() {
     try {
       this.resultado = this.calcular(this.entradaActual);
@@ -73,27 +84,32 @@ class Calculadora {
     }
   }
 
+  // Alternar paréntesis en la entrada
   alternarParentesis() {
     this.agregarAPantalla(this.estaParentesisAbierto ? "(" : ")");
     this.estaParentesisAbierto = !this.estaParentesisAbierto;
   }
 
+  // Borrar el último carácter de la entrada
   borrarUltimaEntrada() {
     this.entradaActual = this.entradaActual.slice(0, -1);
     this.pantalla.value = this.entradaActual;
   }
 
+  // Eliminar una entrada del historial
   eliminarDelHistorial(indice) {
     this.historialEcuaciones.splice(indice, 1);
     this.actualizarHistorial();
   }
 
+  // Actualizar el historial en el DOM
   actualizarHistorial() {
     const listaHistorial = document.getElementById("lista-historial");
     listaHistorial.innerHTML = "";
     this.historialEcuaciones.forEach((entrada, indice) => {
       const elementoLista = document.createElement("li");
 
+      // Elementos de la lista de historial
       const spanEcuacion = document.createElement("span");
       spanEcuacion.textContent = entrada.ecuacion;
       spanEcuacion.style.cursor = "pointer";
@@ -117,6 +133,7 @@ class Calculadora {
     });
   }
 
+  // Inicializar eventos de botones
   inicializarEventos() {
     document
       .getElementById("btn-limpiarPantalla")
@@ -139,8 +156,10 @@ class Calculadora {
   }
 }
 
+// Inicializar la calculadora cuando el DOM está listo
 document.addEventListener("DOMContentLoaded", () => new Calculadora());
 
+// Cambiar al modo oscuro
 const switchModoOscuro = document.getElementById("switch-modo-oscuro");
 let modoOscuroActivado = false;
 
@@ -155,6 +174,7 @@ switchModoOscuro.addEventListener("change", () => {
   }
 });
 
+// Borrar el historial completo
 function borrarHistorial() {
   const listaHistorial = document.getElementById("lista-historial");
   listaHistorial.innerHTML = "";
